62fe68e708c4d9181500785142a5c12b4c015b0e
pubudu2003 <madhus@gmail.com>
2024-12-07 12:41:45 +0530

    create userrepo and user entity

--- src/main/java/com/example/project1/repository/UserRepository.java
+++ src/main/java/com/example/project1/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.example.project1;
+package com.example.project1.repository;
 
+import com.example.project1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
