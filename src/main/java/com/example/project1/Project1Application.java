cdb964f2eae05d7d91339436b3e897c107cefae6
pubudu2003 <madhus@gmail.com>
2024-12-07 12:20:45 +0530

    add modelmapper

--- src/main/java/com/example/project1/Project1Application.java
+++ src/main/java/com/example/project1/Project1Application.java
@@ -1,6 +1,6 @@
 package com.example.project1;
 
-import org.model
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
